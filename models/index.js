b72938756777ab1034ec0e4e3ed0158d4f9666cd
sharkbyte <sgquin@gmail.com>
2022-05-04 22:35:54 -0700

    finished all associations

--- models/index.js
+++ models/index.js
@@ -1,10 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_product",
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "find_product",
+});
 
 // Categories have many Products
 
